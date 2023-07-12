82d752da88d3341b6f4dcf13d5257ce514468df0
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2023-07-12 14:08:59 +0200

    Migrate project to sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	//TODO TEST SLOTS FROM ASYNC SEARCH
+	import { Multiselect } from '../lib/index.js';
+	let stringOptions = [
+		'Select option',
+		'options',
+		'selected',
+		'mulitple',
+		'label',
+		'searchable',
+		'clearOnSelect',
+		'hideSelected',
+		'maxHeight',
+		'allowEmpty',
+		'showLabels',
+		'onChange',
+		'touched'
+	];
+
+	const startObjectOptions = [
+		{ title: 'first', id: 0 },
+		{ title: 'second', id: 1 },
+		{ title: 'third', id: 2 },
+		{ title: 'forth', id: 3 },
+		{ title: 'fifth', id: 4 },
+		{ title: 'six', id: 5 },
+		{ title: 'seventh', id: 6 }
+	];
+
+	const nestnedOptions = [
+		{
+			language: 'Javascript',
+			libs: [
+				{ name: 'Vue.js', category: 'Front-end' },
+				{ name: 'Adonis', category: 'Backend' }
+			]
+		},
+		{
+			language: 'Ruby',
+			libs: [
+				{ name: 'Rails', category: 'Backend' },
+				{ name: 'Sinatra', category: 'Backend' }
+			]
+		},
+		{
+			language: 'Other',
+			libs: [
+				{ name: 'Laravel', category: 'Backend' },
+				{ name: 'Phoenix', category: 'Backend' }
+			]
+		}
+	];
+
+	let objectOptions = nestnedOptions;
+
+	let value: any[] = [];
+	let loading;
+	$: console.log(value);
+
+	function getLimitText(count: number) {
+		return `and ${count} other things`;
+	}
+</script>
+
+<Multiselect
+	showLabels={true}
+	closeOnSelect={false}
+	placeholder="Pick some"
+	options={objectOptions}
+	trackBy="name"
+	label="name"
+	clearOnSelect={false}
+	limit={2}
+	max={10}
+	taggable
+	searchable
+	limitText={getLimitText}
+	deselectLabel="Can't remove this value"
+	multiple
+	groupValues="libs"
+	groupLabel="language"
+	groupSelect={true}
+	small
+/>
+<Multiselect/>
