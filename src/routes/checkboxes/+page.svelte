1d0a2d8bce7a858a529992bbb42e1acace7c5709
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2024-03-05 18:04:13 +0100

    update deps + start work on checkboxes demo

---
+++ src/routes/checkboxes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Multiselect from '$lib/Multiselect.svelte';
+
+	// generate 20 options
+	const options = Array.from({ length: 20 }, (_, i) => ({
+		id: i + 1,
+		label: `Option ${i + 1}`,
+		value: i + 1
+	}));
+</script>
+
+<h1>Checkboxes demo</h1>
+
+<Multiselect {options} label="Select option">
+	<svelte:fragment slot="option" let:option>
+		{JSON.stringify(option)}
+	</svelte:fragment>
+</Multiselect>
